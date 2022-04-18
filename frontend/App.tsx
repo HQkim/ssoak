2f4281330f6dc0c53369df5273b7223f0168c7c8
mandu <alonemint@gmail.com>
2022-04-18 16:27:38 +0900

    feat: Add initial commit

---
+++ frontend/App.tsx
@@ -0,0 +1,38 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./src/store/modules";
+import MainContainer from "./src/components/Pages/mainContainer";
+import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./src/store/modules";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <View style={styles.container}>
+        <MainContainer />
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ frontend/src/components/Pages/mainContainer.tsx
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Main from "./main";
+import {
+  increase,
+  decreaseAsync,
+  increaseByAsync,
+} from "../../store/modules/counter";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/modules";
+import { useDispatch } from "react-redux";
+
+const mainContainer = () => {
+  const value = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  const onIncrease = () => {
+    dispatch(increase());
+  };
+  const onDecrease = () => {
+    dispatch(decreaseAsync());
+  };
+  const onIncreaseBy = (diff: number) => {
+    dispatch(increaseByAsync(diff));
+  };
+
+  return (
+    <Main
+      value={value}
+      onIncrease={onIncrease}
+      onDecrease={onDecrease}
+      onIncreaseBy={onIncreaseBy}
+    />
+  );
+};
+
+export default mainContainer;
+
+const styles = StyleSheet.create({});
