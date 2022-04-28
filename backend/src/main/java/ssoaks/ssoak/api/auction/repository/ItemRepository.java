9b512d6c67dc914789e60fe3753b1c49d915503d
Hyunkyu Kim <hyunkyu001@gmail.com>
2022-04-28 23:51:10 +0900

    [BE] Chore: Fix merge conflict in ItemRepository.java

--- backend/src/main/java/ssoaks/ssoak/api/auction/repository/ItemRepository.java
+++ backend/src/main/java/ssoaks/ssoak/api/auction/repository/ItemRepository.java
@@ -3,10 +3,9 @@ package ssoaks.ssoak.api.auction.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ssoaks.ssoak.api.auction.entity.Item;
 
+import java.util.Optional;
+
 public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositoryCustom {
-
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
 
     Optional<Item> findBySeq (Long itemSeq);
 }
