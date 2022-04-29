c2263f0476ede653727f23391d04796904deb51e
1gj2wo3tjr <1gj2wo3tjr@gmail.com>
2022-04-30 00:24:21 +0900

    #S06P31A207-152 Feat: Create Login Page

--- frontend/src/components/Pages/profileContainer.tsx
+++ frontend/src/components/Pages/profileContainer.tsx
@@ -1,7 +1,12 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
 import React, { useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { instance } from "../../apis/instance";
+import * as Font from "expo-font";
+import KakaoLoginButton from "../Atoms/Buttons/kakaoLoginButton";
+
+const LogoImage = require("../../../assets/loading/loadingImg.jpg");
+const { height: ScreenHeight } = Dimensions.get("window");
 
 type Props = {
   navigation: any;
@@ -11,6 +16,7 @@ type Props = {
 const ProfileContainer = ({ navigation, route }: Props) => {
   const [isLogin, setIsLogin] = useState(false);
   const [accessToken, setAccessToken] = useState("");
+  const [font, setFont] = useState(false);
 
   const getAccessToken = async () => {
     try {
@@ -32,14 +38,24 @@ const ProfileContainer = ({ navigation, route }: Props) => {
     navigation.addListener("focus", () => {
       getAccessToken();
     });
+    async function loadFonts() {
+      await Font.loadAsync({
+        DoHyeonRegular: require("../../../assets/fonts/DoHyeon-Regular.ttf"),
+      });
+      setFont(true);
+    }
+    loadFonts();
   }, [navigation]);
   return (
     <View>
       {isLogin ? (
         <Text>마이프로필페이지</Text>
       ) : (
-        <View style={{ marginTop: 500 }}>
-          <Button title="Kakao Login" onPress={loadKakaoLogin} />
+        <View style={styles.container}>
+          <Image source={LogoImage} style={styles.logoImg} />
+          {font ? <Text style={styles.title}>내 손 안에 경매장</Text> : null}
+          {font ? <Text style={styles.mainTitle}>쏙</Text> : null}
+          <KakaoLoginButton loadKakaoLogin={loadKakaoLogin} />
         </View>
       )}
     </View>
@@ -48,4 +64,29 @@ const ProfileContainer = ({ navigation, route }: Props) => {
 
 export default ProfileContainer;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    backgroundColor: "#719DD7",
+    alignItems: "center",
+  },
+  logoImg: {
+    width: ScreenHeight / 5,
+    height: ScreenHeight / 5,
+    borderRadius: ScreenHeight / 1,
+    marginTop: ScreenHeight / 5,
+    marginBottom: ScreenHeight / 20,
+  },
+  title: {
+    fontSize: ScreenHeight / 30,
+    color: "#ffff",
+    fontFamily: "DoHyeonRegular",
+  },
+  mainTitle: {
+    fontSize: ScreenHeight / 15,
+    color: "#ffff",
+    marginTop: ScreenHeight / 100,
+    marginBottom: ScreenHeight / 20,
+    fontFamily: "DoHyeonRegular",
+  },
+});
