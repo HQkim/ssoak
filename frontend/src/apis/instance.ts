962fd2de05f2ed55d2f05a7257325b6fe6310e5a
1gj2wo3tjr <1gj2wo3tjr@gmail.com>
2022-05-04 14:48:28 +0900

    [FE] Feat: Add axios interceptor

--- frontend/src/apis/instance.ts
+++ frontend/src/apis/instance.ts
@@ -2,11 +2,25 @@ import axios, { AxiosInstance } from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const BaseURL = "http://k6a207.p.ssafy.io:5000/api/v1";
-const accessToken = AsyncStorage.getItem("accessToken");
 
 export const instance: AxiosInstance = axios.create({
   baseURL: `${BaseURL}`,
   headers: {
-    Authorization: `Bearer ${accessToken}`,
+    Authorization: `Bearer ${AsyncStorage.getItem("accessToken")}`,
+    "Content-type": "application/json",
   },
 });
+
+// export const setApiHeaders = () => {
+instance.interceptors.request.use(
+  (config: any) => {
+    config.headers["Authorization"] = `Bearer ${AsyncStorage.getItem(
+      "accessToken"
+    )}`;
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+// };
