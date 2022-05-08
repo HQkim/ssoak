9958494f008ff2cc851cce8afc9b7c0003e88b07
1gj2wo3tjr <1gj2wo3tjr@gmail.com>
2022-05-09 02:36:26 +0900

    #S06P31A207-239 [FE] Feat: Update favorite with APIs

--- frontend/src/components/Templates/favorite.tsx
+++ frontend/src/components/Templates/favorite.tsx
@@ -11,8 +11,19 @@ import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import AuctionTypeTag from "../Atoms/Tags/auctionTypeTag";
 import CompletedTag from "../Atoms/Tags/completedTag";
+import { getFavoriteItems } from "../../apis/ItemApi";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import { likeItem } from "../../apis/autcionApi";
+import { cancelLikeItem } from "../../apis/autcionApi";
 
-type Props = {};
+type Props = {
+  navigation: any;
+  route: object;
+};
+
+type Items = {
+  items: Array<object>;
+};
 
 const randomImage = require("../../../assets/temp.jpg");
 const { height: ScreenHeight } = Dimensions.get("window");
@@ -20,107 +31,138 @@ const { width: ScreenWidth } = Dimensions.get("window");
 
 const Favorite = (props: Props) => {
   const [heart, setHeart] = useState(true);
-  const items: any[] = [
-    {
-      id: 1,
-      title: "SONY ILCE-7M333333",
-      price: 12000,
-      type: "실시간",
-      isCompleted: true,
-      heart: true,
-    },
-    {
-      id: 2,
-      title: "SONY ILCE-7M3",
-      price: 120000,
-      type: "일반",
-      isCompleted: false,
-      heart: true,
-    },
-    {
-      id: 3,
-      title: "SONY ILCE-7M3",
-      price: 120000,
-      type: "실시간",
-      isCompleted: false,
-      heart: true,
-    },
-  ];
+  const [items, setItems] = useState<Items | null | any>([]);
+  const getData = async () => {
+    const response = await getFavoriteItems();
+    setItems(response);
+  };
   const goDetail = () => {
     console.warn("상세목록으로 이동");
     // navigation.navigate("/")
   };
 
-  const pressHeart = () => {
-    setHeart((prev) => !prev);
+  const pressHeart = async (id: number) => {
+    if (heart == true) {
+      setHeart(false);
+      await cancelLikeItem(id);
+    } else {
+      setHeart(true);
+      await likeItem(id);
+    }
   };
+
+  useFocusEffect(
+    React.useCallback(() => {
+      getData();
+      return () => {};
+    }, [])
+  );
+
   return (
     <View>
-      {items.map((item, index) => (
-        <View key={index} style={styles.favListContainer}>
-          <View>
-            <AuctionTypeTag
-              styles={{ tag: styles.auctionTypeTag }}
-              text={item.type}
-            ></AuctionTypeTag>
-          </View>
-          <View style={{ flexDirection: "row", marginTop: 10 }}>
-            <View style={{ flex: 2 }}>
-              <Image
-                source={randomImage}
-                style={{ width: ScreenHeight / 10, height: ScreenHeight / 10 }}
-              />
-            </View>
-            <View style={{ flex: 4 }}>
-              <Text style={{ fontSize: 18 }} numberOfLines={2}>
-                {item.title}
-              </Text>
-              <Text>{item.isCompleted}</Text>
-              <View style={{ flexDirection: "row" }}>
-                {item.isCompleted === true ? (
-                  <CompletedTag
-                    styles={{ tag: styles.completedTypeTag }}
-                    text={"거래완료"}
-                  />
-                ) : (
-                  <Text style={{ fontSize: 15, flex: 1 }}>현재 입찰가 :</Text>
-                )}
-                <TextInput
-                  editable={false}
-                  maxLength={15}
-                  value={item.price
-                    .toString()
-                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                  style={styles.textArea}
-                  textAlign="center"
-                />
+      {items &&
+        items.map((item, index) => (
+          <View key={index} style={styles.favListContainer}>
+            <TouchableOpacity>
+              <View>
+                <AuctionTypeTag
+                  styles={{ tag: styles.auctionTypeTag }}
+                  text={item.auctionType == "LIVE" ? "실시간" : "일반"}
+                ></AuctionTypeTag>
               </View>
-            </View>
-            <View
-              style={{
-                flex: 1,
-                flexDirection: "row",
-                justifyContent: "flex-end",
-              }}
-            >
-              <TouchableOpacity onPress={() => pressHeart}>
-                {item.heart === true ? (
-                  <Ionicons name="heart" size={24} color="#EA759A" />
-                ) : (
-                  <Ionicons name="heart" size={24} color="black" />
-                )}
-              </TouchableOpacity>
-            </View>
+              <View style={{ flexDirection: "row", marginTop: 10 }}>
+                <View style={{ flex: 2 }}>
+                  <Image
+                    source={{ uri: item.imageUrl }}
+                    style={{
+                      width: ScreenHeight / 10,
+                      height: ScreenHeight / 10,
+                    }}
+                  />
+                </View>
+                <View style={{ flex: 6, justifyContent: "space-between" }}>
+                  <Text style={{ fontSize: 18 }} numberOfLines={2}>
+                    {item.title}
+                  </Text>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                    }}
+                  >
+                    <CompletedTag
+                      styles={{ tag: styles.completedTypeTag }}
+                      text={item.isSold == true ? "거래완료" : "진행중"}
+                    />
+                    <Text>참여자 : </Text>
+                    <TextInput
+                      editable={false}
+                      maxLength={3}
+                      value={item.biddingCount
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                      style={styles.textArea}
+                      textAlign="center"
+                    />
+                  </View>
+                </View>
+                <View
+                  style={{
+                    // flex: 1,
+                    flexDirection: "row",
+                    justifyContent: "flex-end",
+                  }}
+                >
+                  <TouchableOpacity onPress={() => pressHeart(item.itemSeq)}>
+                    {heart === true ? (
+                      <Ionicons name="heart" size={24} color="#EA759A" />
+                    ) : (
+                      <Ionicons name="heart-outline" size={24} color="black" />
+                    )}
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  marginTop: ScreenWidth / 30,
+                  justifyContent: "space-between",
+                }}
+              >
+                <View style={{ flexDirection: "row", flex: 1 }}>
+                  <Text>시초가 : </Text>
+                  <TextInput
+                    editable={false}
+                    maxLength={7}
+                    value={item.startPrice
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    style={styles.textArea}
+                    textAlign="center"
+                  />
+                </View>
+                <View style={{ flexDirection: "row", flex: 1, marginLeft: 19 }}>
+                  <Text>{item.isSold == true ? "낙찰가 : " : "입찰가 : "}</Text>
+                  <TextInput
+                    editable={false}
+                    maxLength={7}
+                    value={item.lastPrice
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    style={styles.textArea}
+                    textAlign="center"
+                  />
+                </View>
+              </View>
+              <View
+                style={{
+                  borderBottomColor: "#d7d4d4",
+                  borderBottomWidth: 1,
+                  marginTop: 15,
+                }}
+              ></View>
+            </TouchableOpacity>
           </View>
-          <View
-            style={{
-              borderBottomColor: "#d7d4d4",
-              borderBottomWidth: 1,
-              marginTop: 15,
-            }}
-          ></View>
-        </View>
-      ))}
+        ))}
     </View>
   );
 };
@@ -134,10 +176,10 @@ const styles = StyleSheet.create({
     marginRight: 20,
   },
   textArea: {
-    flex: 1,
     borderRadius: 20,
     borderWidth: 0.5,
     height: 24,
+    width: ScreenWidth / 4,
   },
   auctionTypeTag: {
     fontSize: 15,
