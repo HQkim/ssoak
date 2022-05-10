58e37a4728b0c61e8fa4a5561e0b974b551fceef
1gj2wo3tjr <1gj2wo3tjr@gmail.com>
2022-05-10 10:45:22 +0900

    [FE] Style: Update kakaoProfile in auth.ts

--- frontend/src/apis/auth.ts
+++ frontend/src/apis/auth.ts
@@ -11,26 +11,18 @@ export const kakaoLogin = async (access_code: string) => {
   return response.data;
 };
 
-export const kakaoProfile = async (access_Token: string) => {
+export const kakaoProfile = async () => {
   try {
-    const response = await instance.get("/members/profile", {
-      headers: {
-        Authorization: `Bearer ${access_Token}`,
-      },
-    });
-    // console.log("kakao1---------------------", response.data);
+    const response = await instance.get("/members/profile");
+    console.log(response.data);
     if (response.data.statusCode == 200) {
-      // console.log("kakao2---------------------", response.data);
       return response.data;
     } else if (response.data.statusCode == 401) {
-      // console.log("kakao3---------------------", response.data);
       console.warn("회원 권한 없음");
     } else if (response.data.statusCode == 500) {
-      // console.log("kakao4---------------------", response.data);
       console.warn("내부 서버 에러");
     }
   } catch (err) {
-    // console.log("kakao5---------------------", err);
     console.log(err);
   }
 };
@@ -39,7 +31,7 @@ export const kakaoDelete = async () => {
   try {
     const response = await instance.delete("/members/profile");
     if (response.data.statusCode === 200) {
-      console.log("계정삭제성공");
+      console.log("회원 탈퇴 성공");
     } else if (response.data.statusCode === 401) {
       console.log("회원 권한 없음");
     } else if (response.data.statusCode === 500) {
@@ -51,10 +43,19 @@ export const kakaoDelete = async () => {
     console.log(e);
   }
 };
-export const editKakaoNickname = async (formData) => {
-  const response = await fileInstance.put(
-    "/members/profile/formData",
-    formData
-  );
-  return response.data;
+export const editKakaoProfile = async (formData) => {
+  try {
+    const response = await fileInstance.patch("/members/profile", formData);
+    if (response.data.statusCode === 200) {
+      console.log("프로필 수정 성공");
+    } else if (response.data.statusCode === 401) {
+      console.log("회원 권한 없음");
+    } else if (response.data.statusCode === 409) {
+      console.log("프로필 수정 실패");
+    } else if (response.data.statusCode === 500) {
+      console.log("내부 서버 에러");
+    }
+  } catch (e) {
+    console.log(e);
+  }
 };
