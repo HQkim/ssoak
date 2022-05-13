f471f596226513d7d7222faed0f6a6d0b3826d53
1gj2wo3tjr <1gj2wo3tjr@gmail.com>
2022-05-13 20:38:29 +0900

    [FE] Fix: Fix search filterign error

--- frontend/src/components/Atoms/Typographies/navigatorTextInput.tsx
+++ frontend/src/components/Atoms/Typographies/navigatorTextInput.tsx
@@ -1,3 +1,71 @@
+// import { StyleSheet, Text, View, TextInput, Dimensions } from "react-native";
+// import React, { useState, useEffect } from "react";
+// import { Ionicons } from "@expo/vector-icons";
+// import { searchItem } from "../../../apis/categoryApi";
+
+// type Props = {
+//   style: object;
+//   text: any;
+//   setText: Function;
+//   setItems: any;
+//   page: number;
+//   setPage: Function;
+//   form: any;
+//   setForm: Function;
+// };
+
+// interface Form {
+//   keyword: string;
+//   page: number;
+// }
+
+// const { width: ScreenWidth } = Dimensions.get("window");
+
+// const NavigatorTextInput = (props: Props) => {
+//   const [test, setTest] = useState([]);
+//   const searchKeyword = async (text) => {
+//     console.log(props.form.form);
+//     const result = await searchItem(props.form.form);
+
+//     props.setItems(result);
+//     // props.setPage((prev) => prev + 1);
+//     props.form.form.page = props.page + 1;
+//   };
+
+//   useEffect(() => {
+//     if (props.text) {
+//       props.form.form.keyword = props.text;
+//       props.form.form.page = 1;
+//     }
+//   }, [props.text]);
+
+//   return (
+//     <View>
+//       <TextInput
+//         style={props.style}
+//         onChangeText={(text) => props.setText(text)}
+//         value={props.text}
+//       />
+//       {props.text ? (
+//         <Ionicons
+//           name="search-outline"
+//           size={35}
+//           color="black"
+//           style={{
+//             position: "absolute",
+//             left: ScreenWidth / 2 + 10,
+//             justifyContent: "center",
+//           }}
+//           onPress={() => searchKeyword(props.text)}
+//         />
+//       ) : null}
+//     </View>
+//   );
+// };
+
+// export default NavigatorTextInput;
+
+// const styles = StyleSheet.create({});
 import { StyleSheet, Text, View, TextInput, Dimensions } from "react-native";
 import React, { useState, useEffect } from "react";
 import { Ionicons } from "@expo/vector-icons";
@@ -24,18 +92,18 @@ const { width: ScreenWidth } = Dimensions.get("window");
 const NavigatorTextInput = (props: Props) => {
   const [test, setTest] = useState([]);
   const searchKeyword = async (text) => {
-    console.log(props.form.form);
+    props.setItems([]);
+    props.form.form.page = 1;
     const result = await searchItem(props.form.form);
-
     props.setItems(result);
-    // props.setPage((prev) => prev + 1);
-    props.form.form.page = props.page + 1;
   };
 
   useEffect(() => {
     if (props.text) {
       props.form.form.keyword = props.text;
       props.form.form.page = 1;
+    } else {
+      props.setItems([]);
     }
   }, [props.text]);
 
