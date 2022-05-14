09b3fe1558b3ba4cca199af515a9d31547d79cda
mandu <alonemint@gmail.com>
2022-05-14 19:02:18 +0900

    feat: add licenses & report

--- frontend/src/components/Templates/licenseDetail.tsx
+++ frontend/src/components/Templates/licenseDetail.tsx
@@ -1,17 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Linking } from "react-native";
 import React from "react";
 
 type Props = {};
 
 const LicenseDetail = (props: any) => {
-  console.log(props.route.params);
+  const item = props.route.params.item;
   return (
-    <View>
-      <Text>LicenseDetail</Text>
+    <View style={{ margin: 10 }}>
+      <Text style={{ fontSize: 20 }}>library name : {item.libraryName}</Text>
+      <Text
+        onPress={() => Linking.openURL(item.homepage)}
+        style={{ color: "blue" }}
+      >
+        {item.homepage}
+      </Text>
+      <Text>version : {item.version}</Text>
+      {item.author ? (
+        <>
+          <Text>
+            author: {item.author.name ? item.author.name : "anonymous"}
+          </Text>
+
+          <Text
+            style={{ color: "blue" }}
+            onPress={() => Linking.openURL(`mailto:${item.author?.email!}`)}
+          >
+            {item.author?.email!}
+          </Text>
+        </>
+      ) : null}
+      <View style={{ borderBottomWidth: 1, marginTop: 5, marginBottom: 5 }} />
+      {item._description ? (
+        <Text>description : {item._description}</Text>
+      ) : null}
+      <View style={{ borderBottomWidth: 1, marginTop: 5, marginBottom: 5 }} />
+      <Text>license : {item._license}</Text>
     </View>
   );
 };
 
 export default LicenseDetail;
-
 const styles = StyleSheet.create({});
