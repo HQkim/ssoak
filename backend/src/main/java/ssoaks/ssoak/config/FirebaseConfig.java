3b3e08c2892b2f3f0eca7e710bbe6eee2d6ec220
Hyunkyu Kim <hyunkyu001@gmail.com>
2022-05-17 00:15:12 +0900

    [BE] Feat: Change item title in FirebaseUtil

--- backend/src/main/java/ssoaks/ssoak/config/FirebaseConfig.java
+++ backend/src/main/java/ssoaks/ssoak/config/FirebaseConfig.java
@@ -5,9 +5,11 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
@@ -21,11 +23,11 @@ public class FirebaseConfig {
     @PostConstruct
     public void init(){
         try{
+            InputStream inputStream = getClass().getResourceAsStream("/serviceAccountKey.json");
 
-            FileInputStream serviceAccount =
-                    new FileInputStream(filePath);
+//            FileInputStream serviceAccount = new FileInputStream(filePath);
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(GoogleCredentials.fromStream(inputStream))
                     .setDatabaseUrl(databaseUrl)
                     .build();
             FirebaseApp.initializeApp(options);
